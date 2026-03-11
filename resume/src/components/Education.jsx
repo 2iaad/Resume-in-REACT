ced7781d6dbefcc5144b9f5b26830a44a5005717
2iaad <ziadderfoufi77@gmail.com>
2026-03-11 13:54:12 +0000

    [feat]

--- resume/src/components/Education.jsx
+++ resume/src/components/Education.jsx
@@ -1,28 +1,12 @@
+import EducationField from "./EducationField";
+
 export default function Education() {
   return (
     <section>
       <h2 className="text-xl font-semibold mb-1">Education</h2>
       <hr className='mb-2'></hr>
 
-      <div className="space-y-2">
-
-        <div>
-          <h3 className="font-semibold">1337 Coding School – Khouribga</h3>
-          <p className="text-gray-600 text-sm">
-            Computer Science • 11/2023 – 02/2026
-          </p>
-        </div>
-
-        <div>
-          <h3 className="font-semibold">
-            High School – Sharif El Idrissi Benslimane
-          </h3>
-          <p className="text-gray-600 text-sm">
-            Baccalaureate in Physical Sciences • 09/2020 – 06/2021
-          </p>
-        </div>
-
-      </div>
+      <EducationField />
     </section>
   )
 }
--- resume/src/components/EducationField.jsx
+++ resume/src/components/EducationField.jsx
@@ -1,30 +1,31 @@
 export default function EducationField() {
 	const obj = [
-		{degree: "Computer Science",
-		duration: "11/2023 – 02/2026",
-		school: "1337 Coding School – Khouribga"},
-		{degree: "Baccalaureate in Physical Sciences",
-		duration: "09/2020 – 06/2021",
-		school: "High School – Sharif El Idrissi Benslimane"}]
-	
+		{
+			degree: "Computer Science",
+			duration: "11/2023 – 02/2026",
+			school: "1337 Coding School – Khouribga"
+		},
+		{
+			degree: "Baccalaureate in Physical Sciences",
+			duration: "09/2020 – 06/2021",
+			school: "High School – Sharif El Idrissi Benslimane"
+		}]
+
+	const toRender = obj.map((item) => {
+		return (
+			<div>
+				<div className="flex justify-between">
+					<h3 className="font-semibold"> {item.school} </h3>
+					<p className="text-gray-600 text-sm"> {item.duration} </p>
+				</div>
+				<p className="text-gray-600 text-sm"> {item.degree} </p>
+			</div>
+		)
+	})
+
 	return (
 		<div className="space-y-2">
-
-			<div>
-				<h3 className="font-semibold">1337 Coding School – Khouribga</h3>
-				<p className="text-gray-600 text-sm"> {degree} </p>
-				<p className="text-gray-600 text-sm"> {duration} </p>
-			</div>
-
-			<div>
-				<h3 className="font-semibold">
-					High School – Sharif El Idrissi Benslimane
-				</h3>
-				<p className="text-gray-600 text-sm">
-					Baccalaureate in Physical Sciences • 09/2020 – 06/2021
-				</p>
-			</div>
-
+			{toRender}
 		</div>
 	)
 }
