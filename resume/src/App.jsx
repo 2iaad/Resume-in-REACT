62c6bb2164c46f75b098252d6fd23fcffa74f585
2iaad <ziadderfoufi77@gmail.com>
2026-03-15 21:34:34 +0000

    [fix] printing

--- resume/src/App.jsx
+++ resume/src/App.jsx
@@ -12,20 +12,25 @@ import './index.css'
 
 export default function App() {
   return (
-    <div className="flex flex-col items-center bg-gray-100 min-h-screen text-[15px]">
+    // <div className="flex flex-row bg-amber-800">
+    //   <aside className="border-2 w-250">
 
-      <DownloadButton />
-      <div id='resume' className="flex w-[825px] min-h-[1175px] max-h-[1175px] m-6 bg-white shadow-lg overflow-clip">
-        <div className="flex flex-col self-start gap-3 mx-10 my-8 max-h-[1105px] overflow-clip">
-          <Header />
-          <Summary />
-          <Skills />
-          {/* <Experience /> */}
-          <Education />
-          <Projects />
+    //   </aside>
+
+      <div className="flex flex-col items-center w-full min-h-screen text-[15px]">
+        <DownloadButton />
+        <div id='resume' className="flex w-[825px] min-h-[1175px] max-h-[1175px] m-6 bg-white shadow-lg overflow-clip">
+          <div className="flex flex-col self-start gap-3 mx-10 my-8 max-h-[1105px] overflow-clip">
+            <Header />
+            <Summary />
+            <Skills />
+            {/* <Experience /> */}
+            <Education />
+            <Projects />
+          </div>
         </div>
       </div>
-    </div>
+    // </div>
   )
 }
 
